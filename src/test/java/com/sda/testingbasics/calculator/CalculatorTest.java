1ef22e5218652ed42dee40ce77c4d011014b2bb9
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-11-20 10:41:19 +0100

    first empty test

---
+++ src/test/java/com/sda/testingbasics/calculator/CalculatorTest.java
@@ -0,0 +1,13 @@
+package com.sda.testingbasics.calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@Test
+	void shouldAddTwoPositiveNumbers() {
+
+	}
+}
